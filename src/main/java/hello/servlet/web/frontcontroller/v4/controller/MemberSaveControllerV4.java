7668fc8cbcc9550a423cd8118cb18aef28e3b741
Fred Lee <juhee.fred.lee@gmail.com>
2023-06-23 22:02:33 +0900

    Feat: MVC 프레임워크 만들기-실용성 개선-v4 컨트롤러가 ViewName만 반환

---
+++ src/main/java/hello/servlet/web/frontcontroller/v4/controller/MemberSaveControllerV4.java
@@ -0,0 +1,17 @@
+package hello.servlet.web.frontcontroller.v4.controller;
+
+import hello.servlet.domain.Member.Member;
+import hello.servlet.domain.Member.MemberRepository;
+import hello.servlet.web.frontcontroller.v4.ControllerV4;
+
+import java.util.Map;
+
+public class MemberSaveControllerV4 implements ControllerV4 {
+    MemberRepository memberRepository = MemberRepository.getInstance();
+    @Override
+    public String process(Map<String, String> paramMap, Map<String, Object> model) {
+        Member savedMember = memberRepository.save(new Member(paramMap.get("username"), Integer.parseInt(paramMap.get("age"))));
+        model.put("member", savedMember);
+        return "save-result";
+    }
+}
